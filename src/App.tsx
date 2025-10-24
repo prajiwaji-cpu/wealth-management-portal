f545b5e357ace42ad05f32a4a00cb18fba633013
prajiwaji-cpu <prajiwaji@gmail.com>
2025-10-24 14:40:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import FieldOptionsInspector from './FieldOptionsInspector';
 
 function App() {
   return <FieldOptionsInspector />;
 }
+
+export default App;  // ← THIS LINE IS IMPORTANT!
